1d3f42aaab26535869b6d505d4522fc5ec359b41
Simon Kosgei <simonwriter254@gmail.com>
2025-03-23 22:26:12 +0300

    Update 2025_03_05_081115_add_metadata_to_activities_table.php

--- database/migrations/2025_03_05_081115_add_metadata_to_activities_table.php
+++ database/migrations/2025_03_05_081115_add_metadata_to_activities_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('activities', function (Blueprint $table) {
-            $table->json('metadata')->nullable()->after('subject_id');
+            //$table->json('metadata')->nullable()->after('subject_id');
         });
     }
 
